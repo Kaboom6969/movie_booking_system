24b437a9c6700a982cfeadd6a2a41458a8ee559b
BGORE <leezhihao0427@gmail.com>
2025-09-01 19:53:21 +0800

    FrameWork Version 1.11

--- Main Program/Library/movie_booking_framework/__movie_seats_framework__.py
+++ Main Program/Library/movie_booking_framework/__movie_seats_framework__.py
@@ -1,10 +1,9 @@
 import csv #From python standard library
 
 #读取csv档案到某一个二维数组（自己决定）用来把储存的座位表拉出来处理
-def read_movie_seats_list_csv (movie_seats_csv : csv,movie_code : str) -> list:
+def read_movie_seats_list_csv (movie_seats_csv : csv,movie_seat_list :list,movie_code : str) -> None:
     list_found = False
     start_status = False
-    movie_seat_list = []
     try:
         with open(movie_seats_csv, 'r', newline='') as csvfile:
             movie_seat_reader = csv.reader(csvfile)
@@ -23,14 +22,13 @@ def read_movie_seats_list_csv (movie_seats_csv : csv,movie_code : str) -> list:
                     movie_seat_list.append(row[1:])
     except FileNotFoundError:
         raise FileNotFoundError (f"File not found!\nYour file name is {movie_seats_csv}.\nPlease Check The Name!")
-    return movie_seat_list
 
-def backup_file (original_file : csv,new_file : csv) -> None:
-    with open(original_file, 'r', newline='') as ori_file_r, open(new_file, 'w', newline='') as new_file_w:
+def _overwrite_file (overwrited_file : csv,original_file : csv) -> None:
+        with open(original_file, 'r', newline='') as ori_file_r, open(overwrited_file, 'w', newline='') as overwrited_file_w:
             oni_file_reader = csv.reader(ori_file_r)
-            new_file_writer = csv.writer(new_file_w)
+            overwrited_file_writer = csv.writer(overwrited_file_w)
             for row in oni_file_reader:
-                new_file_writer.writerow(row)
+                overwrited_file_writer.writerow(row)
 
 def write_movie_seats_list_csv (movie_seats_csv : csv, movie_seat_list : list, movie_code : str) -> None:
     with open(movie_seats_csv, 'r', newline='') as movie_seats_r, open(f"{movie_seats_csv}.temp","w", newline='') as movie_seats_w:
@@ -52,6 +50,7 @@ def write_movie_seats_list_csv (movie_seats_csv : csv, movie_seat_list : list, m
                 if list_found and row and row[0] == "START":
                     start_status = True
                 movie_seat_writer.writerow(row)
+    _overwrite_file(overwrited_file = movie_seats_csv, original_file = f"{movie_seats_csv}.temp")
 
 
 
@@ -94,13 +93,13 @@ def print_movie_seats_list_as_emojis (movie_seat_list : list) -> None:
 if __name__ == '__main__':
     movie_seats_list_global : list = []
     try:
-        movie_seats_list_global = read_movie_seats_list_csv (movie_seats_csv="movie_seat.csv",movie_code="001")
+        read_movie_seats_list_csv (movie_seats_csv="movie_seat.csv",movie_seat_list=movie_seats_list_global,movie_code="002")
         fill_movie_seats_list (movie_seat_list=movie_seats_list_global, fill_number=1)
         # backup_file(original_file="movie_seat.csv",new_file="movie_seat_backup.csv.temp")
         write_movie_seats_list_csv(movie_seats_csv="movie_seat.csv",movie_seat_list= movie_seats_list_global,movie_code="002")
         # modify_movie_seat(movie_seat_list=movie_seat_list_global, x_axis = 1,y_axis =2,target_number = -1)
         # print_movie_seats_list(movie_seat_list= movie_seat_list_global)
-        #print_movie_seats_list_as_emojis(movie_seat_list= movie_seats_list_global)
+        # print_movie_seats_list_as_emojis(movie_seat_list= movie_seats_list_global)
     except IndexError as e:
         print(e)
     except FileNotFoundError as e:
