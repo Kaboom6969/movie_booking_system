4971f967218960e4ecfc4b879cd8bd8657825ab9
BOGORE_LAPTOP <leezhihao0427@gmail.com>
2025-09-13 02:33:39 +0800

    Customer Version 1.0.0 - added `check_ticket_bought` function (still in test)

--- main_program/Library/role/CHANGELOG.md
+++ main_program/Library/role/CHANGELOG.md
@@ -4,3 +4,8 @@
 ### Added
 ## [1.1.0] - 2025-09-13
 - Initial booking system (basic version, still needs improvement).
+
+## Customer
+## [1.0.0] - 2025-09-13
+### Added
+- added `check_ticket_bought` function (still in test)
--- main_program/Library/role/customer.py
+++ main_program/Library/role/customer.py
@@ -1,6 +1,10 @@
 from movie_booking_system.main_program.Library.movie_booking_framework.movie_seats_framework import *
 from movie_booking_system.main_program.Library.movie_booking_framework.movie_list_framework import *
 
-def check_ticket_bought(booking_data_csv : str,movie_list_csv : str):
+def check_ticket_bought(customer_code : str,booking_data_csv : str,movie_list_csv : str):
     booking_list : list = []
-    read_movie_list_csv(movie_list_csv= booking_data_csv, movie_list= booking_list)
+    read_movie_list_csv(movie_list_csv= booking_data_csv, movie_list= booking_list, movie_code= customer_code)
+    print(booking_list)
+
+if __name__ == '__main__':
+    check_ticket_bought(customer_code = C001,booking_data_csv = "booking_data.csv", movie_list_csv = "movie_list.csv")
