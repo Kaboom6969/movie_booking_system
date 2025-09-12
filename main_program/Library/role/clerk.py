a8f4dbb8a17dd9644c1ea894a80ed18d8b9948c7
wei061027 <wei061027@gmail.com>
2025-09-12 15:34:57 +0800

    Login FrameWork 1.1.0f

---
+++ main_program/Library/role/clerk.py
@@ -0,0 +1,9 @@
+
+def clerk():
+    print('clerk')
+    #订票
+    #read_movie_list_csv(movie_list_csv= "movie_list.csv", movie_list= movie_list_global, movie_code="002")
+    #print(movie_list_global)
+    #取消或修改booking
+    #查看电影列表
+    #打印订单
--- main_program/Library/system_login_framework/Login/login_system/login_system.py
+++ main_program/Library/system_login_framework/Login/login_system/login_system.py
@@ -4,6 +4,9 @@ import os
 import csv
 from operator import truediv
 
+from movie_booking_system.main_program.Library.system_login_framework.Login.role.clerk import clerk
+
+
 
 def get_content(path):
     """
@@ -322,7 +325,7 @@ def role(customer_data, clerk_data, manager_data, technician_data):
     elif role_num == 1:
         flag = login(clerk_data)
         if flag:
-            print("clerk function")
+            clerk()
 
 
     elif role_num == 2:
