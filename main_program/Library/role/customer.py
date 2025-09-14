66924188e0cfb2d11e18330bb066b169be1053c6
BOGORE_LAPTOP <leezhihao0427@gmail.com>
2025-09-15 04:12:50 +0800

    Customer Version 1.1.0 a - Just let you guys see how Spaghetti Code looks like (in `book_movie`)

--- main_program/Library/role/customer.py
+++ main_program/Library/role/customer.py
@@ -41,20 +41,89 @@ def code_range_create(code_list : list,code_location : int) -> list:
         code_range_list.append(row[code_location])
     return code_range_list
 
-#def book_movie(movie_code : str,movie_seats_csv : str) -> None:
+def book_movie(movie_code : str,movie_seats_csv : str) -> None:
+    while True:
+        try:
+            movie_seats : list = movie_list_to_movie_seats_print(movie_code= movie_code,movie_seats_csv= movie_seats_csv)
+            x_range : list = x_range_calculate(movie_seats= movie_seats)
+            y_range : list = y_range_calculate(movie_seats= movie_seats)
+        except Exception as e:
+            raise Exception(f"BOOK MOVIE FAILED!ERROR:{e}\nTHIS IS PROGRAM BUG!\nPLEASE CHECK THE PROGRAM!")
 
-def movie_list_to_movie_seats_print(movie_code : str,movie_seats_csv : str):
+        while True:
+            try:
+                while True:
+                    try:
+                        x_pointer = int(input(f"Please enter the column number (x_axis:{x_range[0]} to {x_range[1]}): "))
+                    except ValueError as e:
+                        raise ValueError("Column number should be a number!")
+                    except IndexError as e:
+                        raise IndexError(f"Column number should be in the range({x_range[0]} - {x_range[1]})")
+
+                    movie_list_to_movie_seats_print(movie_code=movie_code, movie_seats_csv=movie_seats_csv,x_pointer=x_pointer)
+                    break
+
+                while True:
+                    try:
+                        y_pointer = int(input(f"Please enter the row number (y_axis:{y_range[0]} to {y_range[1]}:"))
+                    except ValueError as e:
+                        raise ValueError("Row number should be a number!")
+                    except IndexError as e:
+                        raise IndexError(f"Row number should be in the range({y_range[0]} - {y_range[1]}")
+
+                    movie_list_to_movie_seats_print(movie_code=movie_code, movie_seats_csv=movie_seats_csv, x_pointer= x_pointer, y_pointer= y_pointer)
+                    break
+
+            seats_value = movie_seats_specify_value(movie_seats=movie_seats, x_axis=x_pointer, y_axis=y_pointer)
+            if seats_value in "1": raise ValueError("seats_booked")
+            elif seats_value in "-1": raise ValueError("seats_not_exixts")
+            break
+
+        except ValueError as e:
+            if "seat_booked" in e:
+                print("This seat is already been booked,Please enter the axis again!")
+            if "seats_not_exixts" in e:
+                print("This seat is not exists,Please enter the axis again!")
+
+        while True:
+            try:
+                if seats_value in "0":
+                    while True:
+                        try:
+                            user_input = str(input("Are You (S)ure? OR (C)ancel it ?"))
+                            if user_input.lower() not in "s" and user_input.lower() not in "c":
+                                raise ValueError("user_input value error")
+                            break
+                        except ValueError as e:
+                            print("The command must be S(s) or C(c)!")
+
+                if user_input.lower() in "s":
+                    modify_movie_seats_list(movie_seat_list= movie_seats,x_axis= x_pointer,y_axis= y_pointer,target_number= 1)
+                    update_movie_seats_csv(movie_seats_csv= movie_seats_csv,movie_seats= movie_seats,movie_code= movie_code)
+                    quit = True
+                    break
+                elif user_input.lower() in "c":
+                    break
+        if quit == True:
+            break
+
+
+
+
+
+def movie_list_to_movie_seats_print(movie_code : str,movie_seats_csv : str,x_pointer = -1,y_pointer = -1) -> list:
     movie_seats_list : list = []
     read_movie_seats_csv(movie_seats_csv= movie_seats_csv,movie_seats = movie_seats_list,movie_code= movie_code)
     print()
     print("-" * int(DEFAULT_WIDTH * 1.8))
-    print_movie_seat_as_emojis(movie_seats_list)
+    print_movie_seat_as_emojis(movie_seats_list,x_pointer,y_pointer)
     print("-" * int(DEFAULT_WIDTH * 1.8))
     print(f"Movie Code:{movie_code}")
+    return movie_seats_list
 
 def check_all_movie_list(movie_list_csv : str,movie_seats_csv : str) -> list:
         movie_list_all : list = []
-        read_movie_list_csv (movie_list_csv= movie_list_csv,movie_list= movie_list_all)
+        read_movie_list_csv(movie_list_csv= movie_list_csv,movie_list= movie_list_all)
         movie_list_print_with_format(data_list= movie_list_all,movie_seats_csv= movie_seats_csv)
         return code_range_create(code_list= movie_list_all,code_location= 0)
 
