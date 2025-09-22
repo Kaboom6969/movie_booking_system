1e4d1c66baf6bc821c4369bfe9ce56c3cb0eb88a
BGORE <leezhihao0427@gmail.com>
2025-09-23 03:48:37 +0800

    -sync the main

--- main_program/main.py
+++ main_program/main.py
@@ -1,6 +1,12 @@
-from main_program.Library.movie_booking_framework.data_dictionary import *
+from main_program.Library.movie_booking_framework import data_dictionary_framework as ddf
 from main_program.Library.role import *
+from main_program.Library.role.customer import book_movie_operation
+
 
 #testinggggggggggggggggggggggggggg
 if __name__ == '__main__':
-    init_all_dictionary()
+    ddf.init_all_dictionary()
+    print(ddf.MOVIE_SEATS_DICTIONARY)
+    print(ddf.MOVIE_LIST_DICTIONARY)
+    print(ddf.BOOKING_DATA_DICTIONARY)
+    print(ddf.CINEMA_DEVICE_DICTIONARY)
