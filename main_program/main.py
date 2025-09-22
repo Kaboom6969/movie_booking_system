6b10093440393f147971d4e6f8a73731f8265a32
BGORE <leezhihao0427@gmail.com>
2025-09-22 22:15:06 +0800

    - test for main

---
+++ main_program/main.py
@@ -0,0 +1,6 @@
+from main_program.Library.movie_booking_framework.data_dictionary import *
+from main_program.Library.role import *
+
+#testinggggggggggggggggggggggggggg
+if __name__ == '__main__':
+    init_all_dictionary()
