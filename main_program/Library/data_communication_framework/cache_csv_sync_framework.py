9852e33e08db65e0259a03f75467818f3a77a415
BGORE_LAPTOP <leezhihao0427@gmail.com>
2025-09-23 13:46:16 +0800

    - add the CRUD for the movie seats

--- main_program/Library/data_communication_framework/cache_csv_sync_framework.py
+++ main_program/Library/data_communication_framework/cache_csv_sync_framework.py
@@ -1,7 +1,7 @@
 from main_program.Library.movie_booking_framework import movie_list_framework as mlf
 from main_program.Library.movie_booking_framework import movie_seats_framework as msf
-from main_program.Library.movie_booking_framework import data_dictionary_framework as ddf
-import warnings
+from main_program.Library.cache_framework import data_dictionary_framework as ddf
+
 
 def update_list_sync (list_csv : str,list_data : list,dictionary_cache : dict,code : str,code_location : int = 0) -> None:
     mlf.update_movie_list_csv(movie_list_csv= list_csv,movie_list= list_data,movie_code= code,code_location= code_location)
@@ -26,3 +26,24 @@ def delete_list_sync (list_csv : str,dictionary_cache : dict,code : str,code_loc
 
 def read_list_from_cache (dictionary_cache : dict,code : str = "all",code_location : int = 0) -> list:
     return ddf.read_list_from_cache(dictionary_cache= dictionary_cache,code= code,code_location= code_location)
+
+def update_seats_sync (seats_csv : str,seats_data : list,dictionary_cache,code : str) -> None:
+    msf.update_movie_seats_csv(movie_seats_csv= seats_csv,movie_seats= seats_data,movie_code= code)
+    try:
+        ddf.seat_dictionary_update(dictionary= dictionary_cache,key_to_add= code,seats_data_to_add= seats_data)
+    except Exception as e:
+        raise ValueError(f"Update cache error!\nError : str{e}")
+
+def add_seats_sync (seats_csv : str,seats_data : list,dictionary_cache : dict,code : str,template_code : str) -> None:
+    msf.add_movie_seats_csv(movie_seats_csv= seats_csv,movie_seats= seats_data,movie_code= code,template_code= template_code)
+    try:
+        ddf.seat_dictionary_update(dictionary= dictionary_cache, key_to_add= code, seats_data_to_add= seats_data)
+    except Exception as e:
+        raise ValueError(f"Update cache error!\nError : str{e}")
+
+def delete_seats_sync (seats_csv : str,dictionary_cache : dict,code : str) -> None:
+    msf.delete_movie_seats_csv(movie_seats_csv= seats_csv,movie_code= code)
+    try:
+        ddf.dictionary_delete(dictionary= dictionary_cache,key_to_delete= code)
+    except Exception as e:
+        raise ValueError(f"Update cache error!\nError : str{e}")
