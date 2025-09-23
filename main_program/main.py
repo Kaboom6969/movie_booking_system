e3dc158721b12eb8669c9bf66cdd939f3ed046be
BGORE <leezhihao0427@gmail.com>
2025-09-24 03:24:28 +0800

    - still testing

--- main_program/main.py
+++ main_program/main.py
@@ -1,4 +1,5 @@
 from main_program.Library.cache_framework import data_dictionary_framework as ddf
+from main_program.Library.data_communication_framework import cache_csv_sync_framework as ccsf
 
 #testinggggggggggggggggggggggggggg
 if __name__ == '__main__':
@@ -8,3 +9,7 @@ if __name__ == '__main__':
     print(ddf.BOOKING_DATA_DICTIONARY)
     print(ddf.CINEMA_DEVICE_DICTIONARY)
     print(ddf.TEMPLATE_SEATS_DICTIONARY)
+    print(ddf.MOVIE_TEMPLATE_CODE_DICTIONARY)
+    ccsf.seats_cache_write_to_csv(seats_csv= "movie_seat.csv",seats_dictionary_cache= ddf.MOVIE_SEATS_DICTIONARY
+                                  ,mt_code_dictionary_cache= ddf.MOVIE_TEMPLATE_CODE_DICTIONARY)
+    ccsf.list_cache_write_to_csv(list_csv="movie_list.csv",list_dictionary_cache= ddf.MOVIE_LIST_DICTIONARY)
