315c62e993fd413b9645c71530f1c892a2bec600
BGORE <leezhihao0427@gmail.com>
2025-10-13 22:27:27 +0800

    - fix the bug that manager cannot modify the discount (range set wrong)

--- main_program/Library/role/manager.py
+++ main_program/Library/role/manager.py
@@ -127,8 +127,9 @@ def modify_movie_operation(
             valid_check_func=vc.number_valid_check
         )
         elif "discount" in selected_modify:
-            discount_percent_range: list =[str(i).join("%") for i in range(0,101)]
+            discount_percent_range: list =[str(i) for i in range(0,101)]
             data_to_modify = fu.element_input(element_name= selected_modify,input_range= discount_percent_range)
+            data_to_modify = "%".join(data_to_modify)
         else: data_to_modify = fu.element_input(element_name= selected_modify)
         movie_list_specify[header_to_modify + 1] = data_to_modify
         ccsf.list_dictionary_update(dictionary=movie_list_dict,list_to_add=movie_list_specify)
